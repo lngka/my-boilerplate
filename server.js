207c175db579abefe67ad78c04527c634743ed97
lngka <foxlead@gmail.com>
2017-08-27 15:17:18 +0200

    initiated handlebars-express

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 
 // requirements
 const express  = require("express");
+const path = require("path");
 const dotenv   = require("dotenv");
 const mongoose = require("mongoose");
+const handlebars = require("express-handlebars");
 
 // init environment
 dotenv.load();
@@ -14,7 +16,14 @@ mongoose.connect(process.env.MONGO_URI, {"useMongoClient": true});
 // init app
 const app = express();
 
+// init view engine
+app.set("views", path.join(process.cwd(), "views")); //default views folder
+app.set("view engine", ".hbs"); //default file extension used for looking up views
+app.engine("hbs", handlebars({"extname": ".hbs", "layoutsDir": "views/layouts", "defaultLayout": "main"})); //default engine used for .hbs files
 
+app.get("/", function(req, res) {
+    res.render("index");
+});
 
 // start app
 var port = process.env.PORT;
